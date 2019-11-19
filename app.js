b159238ec8c88c6cf5a8db3ada212999109b3f6d
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-11-18 20:03:59 -0600

    created pseudo class, then changed the class to the ES6 syntax for classes

--- app.js
+++ app.js
@@ -4,48 +4,50 @@
 //     console.log(`Hello! My name is ${person1.name}.`)
 //   }
 // };
-
 // let person2 = {
 //   name: 'Nancy',
 //   sayHello: function() {
 //     console.log(`Hello! My name is ${person2.name}.`)
 //   }
 // };
-
 // let person3 = {
 //   name: 'Anna Beth',
 //   sayHello: function() {
 //     console.log(`Hello! My name is ${person3.name}.`)
 //   }
 // };
-
 // let person4 = {
 //   name: 'Will',
 //   sayHello: function() {
 //     console.log(`Hello! My name is ${person4.name}.`)
 //   }
 // };
-
 // let person5 = {
 //   name: 'Seth',
 //   sayHello: function() {
 //     console.log(`Hello! My name is ${person5.name}.`)
 //   }
 // };
-
-Person(name, city, age);
-{
-  this.name = name;
-  this.city = city;
-  this.age = age;
+class Person {
+  constructor(name, city, age) {
+    this.name = name;
+    this.city = city;
+    this.age = age;
+  }
+  sayHello() {
+    console.log('Hello, my name is ' + this.name + '. I am ' + this.age + ' years old, and I live in ' + this.city + '.');
+  }
 }
-Person.prototype.sayHello = function () {
-  console.log(`Hello, my name is ${name}. I'm ${age} years old, and I live in ${city}.`)
-};
 
 
-Person.sayHello('Kelvin', '60', 'Arab');
-Person.sayHello('Nancy', '58', 'Arab');
-Person.sayHello('Anna Beth', '33', 'Arab');
-Person.sayHello('Will', '31', 'Arab');
-Person.sayHello('Seth', '28', 'Spring Hill');
+
+let p1 = new Person('Kelvin', 'Arab', '60');
+let p2 = new Person('Nancy', 'Arab', '58');
+let p3 = new Person('Anna Beth', 'Arab', '33');
+let p4 = new Person('Will', 'Arab', '31');
+let p5 = new Person('Seth', 'Spring Hill', '28');
+p1.sayHello();
+p2.sayHello();
+p3.sayHello();
+p4.sayHello();
+p5.sayHello();
