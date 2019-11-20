6dac51555730bbdd19455bad846168a3f42c2c01
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-11-19 21:21:51 -0600

    created different classes that inheritated method of first class

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ class Person {
     this.age = age;
   }
   sayHello() {
-    console.log('Hello, my name is ' + this.name + '. I am ' + this.age + ' years old, and I live in ' + this.city + '.');
+    console.log(`Hello, my name is ${this.name}. I'm ${this.age} years old, and I live in ${this.city}.`);
   }
 }
 
@@ -51,3 +51,56 @@ p2.sayHello();
 p3.sayHello();
 p4.sayHello();
 p5.sayHello();
+
+class Vehicle {
+  constructor(manufacturer, wheels) {
+    this.manufacturer = manufacturer;
+    this.wheels = wheels;
+  }
+  aboutVehicle() {
+    console.log('')
+  }
+}
+
+class Trucks extends Vehicle {
+  constructor(manufacturer, wheels, doors, truckBed) {
+    super(manufacturer, wheels);
+    this.doors = doors;
+    this.truckBed = truckBed;
+  }
+  aboutVehicle() {
+    if (this.truckBed) {
+      console.log(`I'm a truck. I'm made by ${this.manufacturer}, and I have ${this.wheels} wheels and ${this.doors} doors.`);
+    }
+  }
+}
+let truck = new Trucks('Ford', 4, 2, true);
+truck.aboutVehicle();
+
+class Sedan extends Vehicle {
+  constructor(manufacturer, wheels, size, mpg) {
+    super(manufacturer, wheels);
+    this.size = size;
+    this.mpg = mpg;
+  }
+  aboutVehicle() {
+    console.log(`I'm a ${this.size} ${this.manufacturer} sedan. I have ${this.wheels} wheels and get ${this.mpg} miles per gallon.`);
+  }
+}
+let sedan = new Sedan('Honda', 4, 'small', 30);
+sedan.aboutVehicle();
+
+class Motorcycle extends Vehicle {
+  constructor(manufacturer, wheels, handlebars, noDoors) {
+    super(manufacturer, wheels);
+    this.handlebars = handlebars;
+    this.noDoors = noDoors;
+  }
+  aboutVehicle() {
+    if (this.handlebars && this.noDoors) {
+      console.log(`I'm a motorcycle made by ${this.manufacturer}, and I have ${this.wheels} wheels.`);
+    }
+  }
+}
+let moto = new Motorcycle('Ducati', 2, true, true);
+moto.aboutVehicle();
